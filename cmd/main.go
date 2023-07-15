d9b534f92cc4afe8e0d0e53dc654ebf48f454d99
= <=>
2023-07-15 05:57:58 +0530

    simple api endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr := ":8080"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		enc := json.NewEncoder(w)
+		w.Header().Set("Content-Type", "application/json")
+		resp := Resp{
+			Language:    "English",
+			Translation: "Hello World!",
+		}
+
+		if err := enc.Encode(resp); err != nil {
+			panic("failed to encode response")
+
+		}
+	})
+
+	log.Println("listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
+type Resp struct {
+	Language    string `json:"language,omitempty"`
+	Translation string `json:"translation,omitempty"`
 }
